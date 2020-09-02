67a27875078739edc866b06c04b347b1675b9322
JuliaGrisales <jgrisales@gmail.com>
2020-09-02 11:13:51 +0200

    Views of contact, about, blog

---
+++ app/Http/Controllers/WebsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebsController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    public function blog()
+    {
+        return view('blog');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
-/*
-Route::get('/aboutme', function () {
-    //return view('welcome');
-    return "About Me";
-});
-
-Route::get('/contactme', function () {
-    //return view('welcome');
-    return "Contact Me";
-});
-
-Route::get('/getintouch', function () {
-    //return view('welcome');
-    return "Post something awesome!";
-});
-
-// URL whit paramethers
-Route::get('/post/{id}/{author}', function ($id, $author) {
-    //return view('welcome');
-    return "Take your post number $id and the author is: $author";
-})->where('author', '[a-zA-Z]+');
 */
 
+Route::get('/', 'WebsController@index');
+Route::get('/home', 'WebsController@index');
+Route::get('/about', 'WebsController@about');
+Route::get('/contact', 'WebsController@contact');
+Route::get('/blog', 'WebsController@blog');
+
 // Using controller for resolve the routes
+/*
 Route::get('/index/{id}', 'ExampleController@index');
 Route::get('/consoleController', 'ConsoleController@console');
 Route::get('/consoleControllerResources', 'ConsoleResourcesController@index');
+*/
